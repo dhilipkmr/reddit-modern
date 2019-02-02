bf781746a7e4553c0310b103794248f7fa914b78
dhilipkmr <dhilip1211@gmail.com>
2019-02-02 21:42:32 +0530

    cach

--- src/pages/routes/routes.js
+++ src/pages/routes/routes.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import NotFound from '../components/NotFound'
 import Home from '../components/Home';
 
 export default () => {
     return (
         <Switch>
-            <Route path='/login' exact render={(props) => <div>Login</div>}/>
             <Route path='/' exact render={(props) => <Home {...props}/>} />
             <Route path='/home' exact render={(props) => <Home {...props} />}/>
-            <Route path='*' render={(props) => <NotFound />}/>
+            <Route path='*' render={(props) => <div>Sorry, this page does not Exist</div>}/>
         </Switch>
     );
 }
--- src/pages/static/serviceWorker.js
+++ src/pages/static/serviceWorker.js
@@ -1,7 +1,19 @@
-self.addEventListener('install', () => {
+self.addEventListener('install', (event) => {
   console.log('Install Event');
+  event.waitUntil(
+    caches.open('static')
+      .then((cacheRef) => {
+        cacheRef.add('/styles/home.css');
+        cacheRef.add('/styles/common.css');
+        cacheRef.add('/client_bundle.js');
+      })
+  );
 });
 
 self.addEventListener('activate', () => {
   console.log('Sw Activated');
+});
+
+self.addEventListener('fetch', () => {
+
 });
